9aa5def3b2ede1c39e6ea4e040f0234fcdd0c4c9
Jacob Vallejo <jacob.vallejo@xaramin.com>
2014-08-20 14:05:41 -0400

    Use try/catch to recover script failures.

--- workflow.groovy
+++ workflow.groovy
@@ -1,6 +1,12 @@
 node('master') {
   git url: "git@github.com:duncanmak/workflow-experiments.git", branch: "trial-one"
   sh "scripts/workspace_status.sh"
-  sh "scripts/script1.sh"
+  try {
+    sh "scripts/script1.sh fail"
+    echo "We shouldn't reach this step."
+  } catch (Throwable t) {
+    t.printStackTrace()
+    echo "A step failed to execute properly"
+  }
   sh "scripts/workspace_status.sh"
 }
