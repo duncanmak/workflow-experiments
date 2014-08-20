564a0e24127462b0114eb409dce2dbb1a5e2a542
Jacob Vallejo <jacob.vallejo@xamarin.com>
2014-08-20 14:58:26 -0400

    Add a try/catch/finally block

--- workflow.groovy
+++ workflow.groovy
@@ -1,12 +1,17 @@
+// This shouldn't fail.
 node('master') {
   git url: "git@github.com:duncanmak/workflow-experiments.git", branch: "trial-one"
   sh "scripts/workspace_status.sh"
   try {
-    sh "scripts/script1.sh fail"
-    echo "We shouldn't reach this step."
+    sh "scripts/script1.sh"
+    sh "scripts/script1.sh 0"
+    echo "Build completed!"
   } catch (Throwable t) {
     t.printStackTrace()
     echo "A step failed to execute properly"
+    throw new Exception("Fatal error during execution, flow cannot continue.")
+  } finally {
+    echo "Cleaning up build..."
+    sh "scripts/workspace_status.sh"
   }
-  sh "scripts/workspace_status.sh"
 }
